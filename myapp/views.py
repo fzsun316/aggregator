0eb73f8d7c54dbed4a4ada1afdfd0134b0293c9c
Fangzhou Sun <fangzhousun@Fangzhous-MacBook-Pro.local>
2016-03-16 23:01:31 -0500

    Restore pycurl

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,6 @@
 from myapp import app
 from models_realtime import RealtimeDelayPrediction
-from models_database import GTFS, Weather, Traffic
+from models_database import GTFS, Weather, Traffic, stdout
 
 @app.route('/')
 @app.route('/index')
@@ -8,8 +8,7 @@ def index():
 	return 'Hello'
 
 
-# @app.route('/test')
-# def test():
-# 	gTFS = GTFS()
-# 	gTFS.requestAllVersions()
-# 	return 'Hello'
+@app.route('/test')
+def test():
+	global stdout
+	return str(stdout)
